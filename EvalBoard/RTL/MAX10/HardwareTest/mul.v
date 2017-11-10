db678b4c350935e952fb0aca968da51e4f1dc8a6
Austin Baylis <abaylis@ufl.edu>
2017-11-10 10:42:43 -0500

    MAX10: Added DES code from HAHA board to HardwareTest

--- EvalBoard/RTL/MAX10/HardwareTest/mul.v
+++ EvalBoard/RTL/MAX10/HardwareTest/mul.v
@@ -23,14 +23,14 @@ module mul
 
     sam_o U1  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz1) , .n(n), .x(x), .e(e_round), .zz(zz1));
     sam_o U2  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz2) , .n(n), .x(x), .e(e_round), .zz(zz2));
-    sam_o U3  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz3) , .n(n), .x(x), .e(e_round), .zz(zz3));
-    sam_o U4  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz4) , .n(n), .x(x), .e(e_round), .zz(zz4));
-    sam_o U5  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz5) , .n(n), .x(x), .e(e_round), .zz(zz5));
-    sam_o U6  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz6) , .n(n), .x(x), .e(e_round), .zz(zz6));
-    sam_o U7  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz7) , .n(n), .x(x), .e(e_round), .zz(zz7));
-    sam_o U8  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz8) , .n(n), .x(x), .e(e_round), .zz(zz8));
-    sam_o U9  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz9) , .n(n), .x(x), .e(e_round), .zz(zz9));
-    sam_o U10 (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz10), .n(n), .x(x), .e(e_round), .zz(zz10));
+    //sam_o U3  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz3) , .n(n), .x(x), .e(e_round), .zz(zz3));
+    //sam_o U4  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz4) , .n(n), .x(x), .e(e_round), .zz(zz4));
+    //sam_o U5  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz5) , .n(n), .x(x), .e(e_round), .zz(zz5));
+    //sam_o U6  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz6) , .n(n), .x(x), .e(e_round), .zz(zz6));
+    //sam_o U7  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz7) , .n(n), .x(x), .e(e_round), .zz(zz7));
+    //sam_o U8  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz8) , .n(n), .x(x), .e(e_round), .zz(zz8));
+    //sam_o U9  (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz9) , .n(n), .x(x), .e(e_round), .zz(zz9));
+    //sam_o U10 (.clk(clk), .z( (cnt[2:0]==3'b000)? 64'h0000000000000001:zz10), .n(n), .x(x), .e(e_round), .zz(zz10));
     
 
     divi Ud(.clk(clk_o), .clkout(clk));
@@ -45,10 +45,10 @@ module mul
     end
 
 
-    ram64 Ur [1:10](
+    ram64 Ur [1:2](
     .address(cnt),
     .clock(clk),
-    .data({zz1, zz2, zz3, zz4, zz5, zz6, zz7, zz8, zz9, zz10}),
+    .data({zz1, zz2}),//, zz3, zz4, zz5}),//, zz6, zz7, zz8, zz9, zz10}),
     .wren(1'b1),
     .q());
 
